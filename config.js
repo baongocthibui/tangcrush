8002b0b2cc304d9059355d083304f9cb26463a40
baongocthibui <113125189+baongocthibui@users.noreply.github.com>
2022-09-08 23:39:33 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Vni à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao anh cứ đi nhầm đường
+    con đường lên 1500 khó tìm thế à?
+    btnIntro: 'hihi',
+    title: 'Mấy hôm nay Vni hắt hơi xổ mũi làm con dân rất lo lắng 😙',
+    desc: 'Vậy Vni à, chỉ là chỉnh hay Vni chuẩn bị die cmnlr Vni ',
+    btnYes: 'VNi: anh chỉnh trêu tí thôi, mai anh lại hồi nha em<33',
+    btnNo: 'Vni:,Xin lỗi em, mẹ anh bắt về 1k cơ :3',
+    question:'index index thôi cứ từ từ vượt đỉnh. Index index thật là là chiều mình. index bạn của mọi nhà <3',
+    btnReply: 'Gửi cho VNi <3',
+    reply: 'Chỉnh thì chỉnh mà yêu thì vẫn yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'mình tạm chia tay nhau nhé em, chỉnh chút thôi, Qua tháng 10 mình gặp lại.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
